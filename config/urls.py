a434d83a89b08d1f05791e27286206ea5edd502e
Joabson <joabson.nascimento.117@ufrn.edu.br>
2021-12-08 09:32:53 -0300

    removes mandatory category in product registration

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 from django.conf.urls.static import static
 from django.conf import settings
